6822e67734ce4515a007ca001c736fbd432d5a73
akhilakkala <135016153+akhilakkala@users.noreply.github.com>
2024-01-20 22:23:29 +0530

    Add files via upload

---
+++ greatest_Of_Two_Numbers.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the first number : "))
+n2 = int(input("Enter the second number : "))
+
+if n1>n2:
+    print("n1 =  " +str(n1)+ " is the greatest among " + str(n1) + " and " + str(n2))
+else:
+    print("n2 =  " +str(n2)+ " is the gratest among " +  str(n1) + " and " + str(n2))
